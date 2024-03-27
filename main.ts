95ee6a4a8597b59c7967e1384ff00612d03f4933
salmandanish03 <162048472+salmandanish03@users.noreply.github.com>
2024-03-28 00:16:09 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,42 @@
+let email: string = "salmandanish03@gmail.com";
+let password:string = "Sd1984"
+
+if (email === "salmandanish03@gmail.com" && password === "sd1984"){
+    console.log("You have successfully logged in");
+}
+else{
+    console.log("Your provided password is incorrect please try again");
+}
+
+// Grading system coding
+
+let percentage: number = 61
+if (percentage >= 80 && percentage <= 89.99) {
+    console.log ("Your Grade is A+");
+} 
+else if(percentage >= 70 && percentage <= 79.99) {
+    console.log ("You Grade is A");
+}
+
+else console.log ("You are just pass");
+
+// Weather (a)
+
+let raining = true;
+if (raining = true){
+    console.log ("you should wear a raincoat");
+}
+else ("you should wear a sunGlass");
+
+
+// Weather (b)
+
+let Weather : string = "Thundersdom";
+if (Weather = "Cloudy"){
+    console.log("A person should wear a rainCoat");
+}
+else if (Weather === "Cloudy"){
+    console.log("A person should wear a light jacket");
+}
+else console.log("A person should wear a sunGlasses");
+
